396c4673785d7f6fcf53354cabf14383ef929e6e
Satya Rama Aditya Pinapala <psraditya30@codeaurora.org>
2020-10-09 09:22:36 -0700

    input: ft3418_i2c: Queue touch suspend work on workqueue
    In case of continuous panel BLANK and UNBLANK, there is a possibility
    of a race condition in the current sequence of operations. This change
    ensures that suspend work is also queued on the same workqueue as the
    resume work to ensure no race around and removes the need for having to
    cancel touch resume work during a panel blank.
    
    Change-Id: I1e0c64a7cd3e001c7224b2ea357877a45c1116e4
    Signed-off-by: ppadasal <quic_ppadasal@quicinc.com>
    [fiqri19102002: Port to ft3418_i2c driver]
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>
    Signed-off-by: mrsrimar22 <mrsrimar22@gmail.com>

--- drivers/input/touchscreen/ft3418_i2c/focaltech_core.c
+++ drivers/input/touchscreen/ft3418_i2c/focaltech_core.c
@@ -1296,13 +1296,19 @@ static void fts_resume_work(struct work_struct *work)
 	fts_ts_resume(ts_data->dev);
 }
 
+static void fts_suspend_work(struct work_struct *work)
+{
+	struct fts_ts_data *ts_data = container_of(work, struct fts_ts_data,
+								suspend_work);
+
+	fts_ts_suspend(ts_data->dev);
+}
+
 static int drm_notifier_callback(struct notifier_block *self,
 				 unsigned long event, void *data)
 {
 	struct drm_notify_data *evdata = data;
 	int *blank = NULL;
-	struct fts_ts_data *ts_data = container_of(self, struct fts_ts_data,
-								drm_notif);
 
 	if (!evdata) {
 		FTS_ERROR("evdata is null");
@@ -1327,8 +1333,7 @@ static int drm_notifier_callback(struct notifier_block *self,
 		break;
 	case DRM_BLANK_POWERDOWN:
 		if (DRM_EARLY_EVENT_BLANK == event) {
-			cancel_work_sync(&fts_data->resume_work);
-			fts_ts_suspend(ts_data->dev);
+			queue_work(fts_data->ts_workqueue, &fts_data->suspend_work);
 		} else if (DRM_EVENT_BLANK == event) {
 			FTS_INFO("suspend: event = %lu, not care\n", event);
 		}
@@ -1479,6 +1484,7 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
 
 	if (ts_data->ts_workqueue) {
 		INIT_WORK(&ts_data->resume_work, fts_resume_work);
+		INIT_WORK(&ts_data->suspend_work, fts_suspend_work);
 	}
 
 #if defined(CONFIG_PM) && FTS_PATCH_COMERR_PM
