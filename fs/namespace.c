237231a716edf94816a70ad62adaea1aa67fa291
Al Viro <viro@zeniv.linux.org.uk>
2021-01-04 15:25:34 -0500

    BACKPORT: umount(2): move the flag validity checks first
    Unfortunately, there's userland code that used to rely upon these
    checks being done before anything else to check for UMOUNT_NOFOLLOW
    support.  That broke in c762edd9b055 ("BACKPORT: fs: refactor ksys_umount").
    Separate those from the rest of checks and move them to ksys_umount();
    unlike everything else in there, this can be sanely done there.
    
    Reported-by: Sargun Dhillon <sargun@sargun.me>
    Fixes: c762edd9b055 ("BACKPORT: fs: refactor ksys_umount")
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: mrsrimar22 <mrsrimar22@gmail.com>
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1649,8 +1649,6 @@ static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
 
-	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
-		return -EINVAL;
 	if (!may_mount())
 		return -EPERM;
 	if (path->dentry != path->mnt->mnt_root)
@@ -1665,6 +1663,7 @@ static int can_umount(const struct path *path, int flags)
 	return 0;
 }
 
+// caller is responsible for flags being sane
 int path_umount(struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
@@ -1686,6 +1685,10 @@ int ksys_umount(char __user *name, int flags)
 	struct path path;
 	int ret;
 	int lookup_flags = 0;
+
+	// basic validity checks done first
+	if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
+		return -EINVAL;
 
 	if (!(flags & UMOUNT_NOFOLLOW))
 		lookup_flags |= LOOKUP_FOLLOW;
