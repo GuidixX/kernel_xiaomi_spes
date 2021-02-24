13af0b2e2e1edd84e255fd9e6b8c5332fe1a63e3
Wonhyuk Yang <vvghjk1234@gmail.com>
2021-02-24 12:09:36 -0800

    mm/compaction: fix misbehaviors of fast_find_migrateblock()
    In the fast_find_migrateblock(), it iterates ocer the freelist to find the
    proper pageblock.  But there are some misbehaviors.
    
    First, if the page we found is equal to cc->migrate_pfn, it is considered
    that we didn't find a suitable pageblock.  Secondly, if the loop was
    terminated because order is less than PAGE_ALLOC_COSTLY_ORDER, it could be
    considered that we found a suitable one.  Thirdly, if the skip bit is set
    on the page block and we goto continue, it doesn't check nr_scanned.
    Fourthly, if the page block's skip bit is set, it checks that page block
    is the last of list, which is unnecessary.
    
    Link: https://lkml.kernel.org/r/20210128130411.6125-1-vvghjk1234@gmail.com
    Fixes: 70b44595eafe9 ("mm, compaction: use free lists to quickly locate a migration source")
    Signed-off-by: Wonhyuk Yang <vvghjk1234@gmail.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1634,6 +1634,7 @@ static unsigned long fast_find_migrateblock(struct compact_control *cc)
 	unsigned long pfn = cc->migrate_pfn;
 	unsigned long high_pfn;
 	int order;
+	bool found_block = false;
 
 	/* Skip hints are relied on to avoid repeats on the fast search */
 	if (cc->ignore_skip_hint)
@@ -1676,7 +1677,7 @@ static unsigned long fast_find_migrateblock(struct compact_control *cc)
 	high_pfn = pageblock_start_pfn(cc->migrate_pfn + distance);
 
 	for (order = cc->order - 1;
-	     order >= PAGE_ALLOC_COSTLY_ORDER && pfn == cc->migrate_pfn && nr_scanned < limit;
+	     order >= PAGE_ALLOC_COSTLY_ORDER && !found_block && nr_scanned < limit;
 	     order--) {
 		struct free_area *area = &cc->zone->free_area[order];
 		struct list_head *freelist;
@@ -1691,7 +1692,11 @@ static unsigned long fast_find_migrateblock(struct compact_control *cc)
 		list_for_each_entry(freepage, freelist, lru) {
 			unsigned long free_pfn;
 
-			nr_scanned++;
+			if (nr_scanned++ >= limit) {
+				move_freelist_tail(freelist, freepage);
+				break;
+			}
+
 			free_pfn = page_to_pfn(freepage);
 			if (free_pfn < high_pfn) {
 				/*
@@ -1700,12 +1705,8 @@ static unsigned long fast_find_migrateblock(struct compact_control *cc)
 				 * the list assumes an entry is deleted, not
 				 * reordered.
 				 */
-				if (get_pageblock_skip(freepage)) {
-					if (list_is_last(freelist, &freepage->lru))
-						break;
-
+				if (get_pageblock_skip(freepage))
 					continue;
-				}
 
 				/* Reorder to so a future search skips recent pages */
 				move_freelist_tail(freelist, freepage);
@@ -1713,13 +1714,8 @@ static unsigned long fast_find_migrateblock(struct compact_control *cc)
 				update_fast_start_pfn(cc, free_pfn);
 				pfn = pageblock_start_pfn(free_pfn);
 				cc->fast_search_fail = 0;
+				found_block = true;
 				set_pageblock_skip(freepage);
-				break;
-			}
-
-			if (nr_scanned >= limit) {
-				cc->fast_search_fail++;
-				move_freelist_tail(freelist, freepage);
 				break;
 			}
 		}
@@ -1732,9 +1728,10 @@ static unsigned long fast_find_migrateblock(struct compact_control *cc)
 	 * If fast scanning failed then use a cached entry for a page block
 	 * that had free pages as the basis for starting a linear scan.
 	 */
-	if (pfn == cc->migrate_pfn)
+	if (!found_block) {
+		cc->fast_search_fail++;
 		pfn = reinit_migrate_pfn(cc);
-
+	}
 	return pfn;
 }
 
