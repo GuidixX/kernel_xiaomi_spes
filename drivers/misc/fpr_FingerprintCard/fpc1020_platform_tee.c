d7e8559db7f3ea4f29261f8af9ec05ba91ef1f94
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-05 19:49:50 +0530

    input/fp: fpc/goodix: Switch to async probe
     * slightly reduces boot times
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/misc/fpr_FingerprintCard/fpc1020_platform_tee.c
+++ drivers/misc/fpr_FingerprintCard/fpc1020_platform_tee.c
@@ -590,6 +590,7 @@ static struct platform_driver fpc1020_driver = {
 	.driver = {
 		.name	= "fpc1020",
 		.of_match_table = fpc1020_of_match,
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 	.probe	= fpc1020_probe,
 	.remove	= fpc1020_remove,
