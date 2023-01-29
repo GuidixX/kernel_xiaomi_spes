d6e475bdd1ecd5f344be88178a4b6c9dd41ea194
Cyber Knight <cyberknight755@gmail.com>
2023-01-30 01:10:13 +0700

    input: ft3418_i2c: Simplify event key reporting
    We would want all gestures to be reported equally
    instead of just double tap and single tap
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: mrsrimar22 <mrsrimar22@gmail.com>

--- drivers/input/touchscreen/ft3418_i2c/focaltech_gesture.c
+++ drivers/input/touchscreen/ft3418_i2c/focaltech_gesture.c
@@ -301,14 +301,9 @@ static void fts_gesture_report(struct input_dev *input_dev, int gesture_id)
 		gesture = -1;
 		break;
 	}
+
 	/* report event key */
-	if ((gesture == KEY_GESTURE_AOD) && fts_data->aod_changed) {
-		FTS_DEBUG("Gesture Code=%d", gesture);
-		input_report_key(input_dev, gesture, 1);
-		input_sync(input_dev);
-		input_report_key(input_dev, gesture, 0);
-		input_sync(input_dev);
-	} else if ((gesture == KEY_GESTURE_U) && fts_data->gesture_mode) {
+        if (gesture != 1) {
 		FTS_DEBUG("Gesture Code=%d", gesture);
 		input_report_key(input_dev, gesture, 1);
 		input_sync(input_dev);
