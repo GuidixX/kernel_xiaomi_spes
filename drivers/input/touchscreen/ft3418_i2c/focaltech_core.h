70f9ba90c4875f87598153404b0750d3a8da0088
Cyber Knight <cyberknight755@gmail.com>
2023-01-30 01:04:05 +0800

    input: ft3418_i2c: Implement double tap attr
    - This is in accordance with our modernized DT2W implementation.
    - It sets double_tap_pressed to 1 if double click is detected.
    - Notify sysfs of the double_tap_pressed node change.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/input/touchscreen/ft3418_i2c/focaltech_core.h
+++ drivers/input/touchscreen/ft3418_i2c/focaltech_core.h
@@ -225,6 +225,7 @@ struct fts_ts_data {
 	struct pinctrl_state *pins_release;
 #endif
 	struct notifier_block drm_notif;
+        int double_tap_pressed;
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
 	//u8 palm_sensor_switch;
 	//bool palm_sensor_changed;
