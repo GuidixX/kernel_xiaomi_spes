8150cedec77cdf0e71da1dceec52523d874a1892
Swami Reddy Reddy <quic_swamired@quicinc.com>
2024-07-24 18:53:36 +0530

    msm: camera: sensor: TOCTOU error handling
    - Change to dereference s_ctrl only after proper
      NULL Dereference Check.
    
    CRs-Fixed: 3875406
    Change-Id: I8e2c717b22efff2a7d6503d38c048e30eff230da
    Signed-off-by: Swami Reddy Reddy <quic_swamired@quicinc.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1615,11 +1615,6 @@ static struct cpuhp_step cpuhp_hp_states[] = {
 		.startup.single		= NULL,
 		.teardown.single	= smpcfd_dying_cpu,
 	},
-	[CPUHP_AP_HRTIMERS_DYING] = {
-		.name			= "hrtimers:dying",
-		.startup.single		= hrtimers_cpu_starting,
-		.teardown.single	= hrtimers_cpu_dying,
-	},
 	/* Entry state on starting. Interrupts enabled from here on. Transient
 	 * state for synchronsization */
 	[CPUHP_AP_ONLINE] = {
