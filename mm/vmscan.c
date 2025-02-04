43d2d6d214a5b5f7828870000fcd91b0f9e29cd8
Kanishk <kanishkthederp@gmail.com>
2025-02-04 22:37:20 +0530

    mm: Set swappiness to `80`
    • AOSP’s Lighter Memory Usage:
       - Fewer background services → Less need to kill apps.
       - Swapping to zRAM is efficient and avoids UFS wear.
    
    • Improved Multitasking:
       - Keeps more apps cached in RAM/zRAM, reducing app reloads.
    
    • Balanced for 4GB RAM:
       - With `watermark_scale_factor = 20` set in previous commit,
         the kernel delays memory reclaim.
       - `swappiness=80` ensures swap is used before OOM killer triggers.
    
    • UFS Optimization:
       - UFS 3.1/2.2/2.1... handles swap I/O faster than eMMC, minimizing lag.
    
    Change-Id: 2d983279a3ed0feaf4b528dbc3471b48ca6d9998
    Signed-off-by: Kanishk <kanishkthederp@gmail.com>
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -176,7 +176,7 @@ int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 100;
+int vm_swappiness = 80;
 
 #ifdef CONFIG_OPLUS_MM_HACKS
 /*
