02090f249113137fe93197350ab0cd90f4af2545
Guidix <guidixm@gmail.com>
2025-02-26 23:01:40 +0200

    defconfig: Bump kernel version to v2.0

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Copyright (C) 2024 GuidixX
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="TheSilentDragon-V1.0-$(date '+%Y%m%d-%H%M').zip"
+ZIPNAME="TheSilentDragon-V2.0-$(date '+%Y%m%d-%H%M').zip"
 TC_DIR="$(pwd)/tc/clang-neutron"
 AK3_DIR="$(pwd)/android/AnyKernel3"
 DEFCONFIG="vendor/spes-perf_defconfig"
